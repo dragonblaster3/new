91076429f72953cc23ff953324d076ac887c41e0
dragonblaster3 <siddantkva8a@gmail.com>
2022-06-27 23:54:47 +0530

    first

---
+++ data_pipelines/example.py
@@ -0,0 +1,4 @@
+import webbrowser
+
+url="https://www.kuhoo.com/"
+webbrowser.open(url)
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='data_pipelines',
+    version='0.0.1',
+    description='data_pipelines package from github repo to build data pipelines from all kinds of db',
+    url='git@github.com:Yue1Harriet1Huang/data_pipelines.git',
+    author='Yue Harriet Huang',
+    author_email='dariahuangster@gmail.com',
+    license='unlicense',
+    packages=['data_pipelines'],
+    zip_safe=False
+)
